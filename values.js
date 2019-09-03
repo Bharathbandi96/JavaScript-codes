5176eebe68ddfb3cbaa9376ef60f233ba4c6888b
Bharathbandi96 <bharathbandi96@gmail.com>
2019-09-03 11:50:20 +0530

    1 file modified

--- values.js
+++ values.js
@@ -1,25 +1,25 @@
-exports.values = function f(){
+exports.values = function f() {
 
   function MyNum(n) {
-  this.number = n;
-}
+    this.number = n;
+  }
 
-MyNum.prototype.valueOf = function() {
-  return ` Number ${this.number}'s multiple of 10 is : `+this.number*10;
-};
+  MyNum.prototype.valueOf = function () {
+    return ` Number ${this.number}'s multiple of 10 is : ` + this.number * 10;
+  };
 
-const obj = new MyNum(5);
+  const obj = new MyNum(5);
 
-console.log(obj.valueOf());
+  console.log(obj.valueOf());
 
 
-const object1 = {
-   a : 34,
-   2 : 'hello',
-   c : true,
-   5 : null
-}
+  const object1 = {
+    a: 34,
+    2: 'hello',
+    c: true,
+    5: null
+  }
 
-console.log(Object.values(object1));
-console.log(Object.keys(object1));
+  console.log(Object.values(object1));
+  console.log(Object.keys(object1));
 }
