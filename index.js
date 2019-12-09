d76ed55697476c9656c96fec06304c5ff268ccdb
Bharathbandi96 <bharathbandi96@gmail.com>
2019-12-09 15:03:41 +0530

    1 file modified

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ import {ArrayFrom} from './Examples/arrayFrom'
 //console.log(arraySome());
 //console.log(arraySplice());
 //console.log(ObjectExp());
-console.log(deepMerge());
+// console.log(deepMerge());
 //console.log(ABC());
 // console.log(Exp());
 //console.log(MapReduce());
